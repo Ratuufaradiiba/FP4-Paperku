4b03af371068e0ddd09da8da9dbc344f2d1be1bc
Dimas Wahyudi <112837056+dimaswahyu12@users.noreply.github.com>
2022-11-13 20:21:53 +0700

    Update tables.blade.php

--- resources/views/admin/tables.blade.php
+++ resources/views/admin/tables.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.index')
 @section('content')
 <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-    <div class="container-fluid py-4">
+    <div class="container-fluid py-4"> 
         <div class="row">
             <div class="col-12">
                 <div class="card mb-4">
