497822b7701fd7cecac26a3d9a7d797747db7025
Dimas Wahyudi <112837056+dimaswahyu12@users.noreply.github.com>
2022-11-16 06:20:46 +0700

    update

---
+++ resources/profile/form.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.index')
+@section('content')
+<section class="section">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Form Author</h5>
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <strong>Whoops!</strong> Terjadi Kesalahan saat input data<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+                    <form class="row g-3" method="POST" action="{{ route('author.store')}}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="col-6">
+                            <label for="inputNanme4" class="form-label">Masukan Nama Author</label>
+                            <input type="text" class="form-control" name="nama" value="{{ old('nama') }}">
+                            @error('nama')
+                                <p class="text text-danger mb-0">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="col-6">
+                            <label for="inputNanme4" class="form-label">Masukan Username</label>
+                            <input type="text" class="form-control" name="username" value="{{ old('username') }}">
+                            @error('username')
+                                <p class="text text-danger mb-0">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="col-6">
+                            <label for="inputNanme4" class="form-label">Masukan E-mail</label>
+                            <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                            @error('email')
+                                <p class="text text-danger mb-0">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="col-6">
+                            <label for="inputNanme4" class="form-label">Masukan Foto</label>
+                            <input type="file" class="form-control" name="foto" accept=".png,.jpg,.jpeg">
+                            @error('foto')
+                                <p class="text text-danger mb-0">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                            <button type="reset" class="btn btn-secondary">Batal</button>
+                        </div>
+                    </form><!-- Vertical Form -->
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
