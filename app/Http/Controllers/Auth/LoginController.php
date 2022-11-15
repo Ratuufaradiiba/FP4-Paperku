c776c298bc5037962a26816389eff5383e3ca39c
Rizky Darms <rizky.darms55@gmail.com>
2022-11-16 02:56:13 +0700

    Nambahin laravel ui/authLoginRegis

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use GrahamCampbell\ResultType\Success;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers; // Untuk menghandle Login berdasarkan ROLE
+    protected function authenticated() // ketika user berhasil login, return
+    {
+        if (auth()->user()->role === 'admin') {
+            return redirect('/backend')->with('success', 'Anda telah berhasil login');
+        } else {
+            return redirect('/')->with('success', 'Anda telah berhasil login');
+        }
+    }
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
--- resources/views/frontend/layouts/partials/script.blade.php
+++ resources/views/frontend/layouts/partials/script.blade.php
@@ -3,14 +3,25 @@
 
 <script src="{{ asset('landingpage/plugins/jQuery/jquery.min.js') }}"></script>
 
-  <script src="{{ asset('landingpage/plugins/bootstrap/bootstrap.min.js') }}"></script>
+<script src="{{ asset('landingpage/plugins/bootstrap/bootstrap.min.js') }}"></script>
 
-  <script src="{{ asset('landingpage/plugins/slick/slick.min.js') }}"></script>
+<script src="{{ asset('landingpage/plugins/slick/slick.min.js') }}"></script>
 
-  <script src="{{ asset('landingpage/plugins/instafeed/instafeed.min.js') }}"></script>
-  
+<script src="{{ asset('landingpage/plugins/instafeed/instafeed.min.js') }}"></script>
 
+{{-- sweet alert --}}
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+@if (session('success'))
+    <script>
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: '{{ session('success') }}',
+            showConfirmButton: true,
+            timer: 1500
+        })
+    </script>
+@endif
 
-  <!-- Main Script -->
-  <script src="{{ asset('landingpage/js/script.js') }}"></script>
-  
+<!-- Main Script -->
+<script src="{{ asset('landingpage/js/script.js') }}"></script>
